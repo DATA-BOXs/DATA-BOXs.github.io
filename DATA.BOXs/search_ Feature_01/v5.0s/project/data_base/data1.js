31e3b95b4b4442da7d1bd94d46b331970922ab31
DATA.BOXs <satax726@gmail.com>
2024-09-29 17:32:38 +0900

    Update data1.js

--- DATA.BOXs/search_ Feature_01/v5.0s/project/data_base/data1.js
+++ DATA.BOXs/search_ Feature_01/v5.0s/project/data_base/data1.js
@@ -1,4 +1,7 @@
-export const data1 = [
-    { title: 'JavaScript', description: '.data01.js' },
-    { title: 'あいうえお', description: 'あい' }
+export const testdata1 = [
+    { title: 'JavaScriptの基本', description: 'JavaScriptの基本的な使い方について学びます。', link: 'https://example.com/javascript' },
+    { title: 'HTMLの基礎', description: 'HTMLの基本を理解し、ウェブページを作成します。', link: 'https://example.com/html' },
+    { title: 'CSSのスタイリング', description: 'CSSを使ってウェブページのデザインを改善します。', link: 'https://example.com/css' },
+    { title: 'test', description: 'test', link: 'https://example.com/css' },
+    { title: 'APIの使い方', description: 'APIを使って外部データを取得する方法を学びます。', link: 'https://example.com/api' }
 ];
