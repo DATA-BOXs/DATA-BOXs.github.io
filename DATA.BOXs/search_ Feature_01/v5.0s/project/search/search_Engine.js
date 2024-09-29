ed7621f94832c2a275f4442983f16b1b7cb6d2bf
DATA.BOXs <satax726@gmail.com>
2024-09-29 17:23:35 +0900

    Update search_Engine.js

--- DATA.BOXs/search_ Feature_01/v5.0s/project/search/search_Engine.js
+++ DATA.BOXs/search_ Feature_01/v5.0s/project/search/search_Engine.js
@@ -1,6 +1,7 @@
 import { data1 } from '../data_base/data1.js';
 import { data2 } from '../data_base/data2.js';
 import { data3 } from '../data_base/data3.js';
+import { testdata1 } from '../data_base/testdata1.js';
 
 // すべてのデータを一つの配列にまとめる
 const data = [...data1, ...data2, ...data3];
