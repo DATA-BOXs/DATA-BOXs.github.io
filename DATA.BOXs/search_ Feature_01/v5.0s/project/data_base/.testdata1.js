20ddb27e11b6641cc72010e38a5281bb40bf88d8
DATA.BOXs <satax726@gmail.com>
2024-09-29 17:59:07 +0900

    Delete DATA.BOXs/search_ Feature_01/v5.0s/project/data_base/.testdata1.js

--- DATA.BOXs/search_ Feature_01/v5.0s/project/data_base/.testdata1.js
+++
@@ -1,7 +0,0 @@
-export const testdata1 = [
-    { title: 'testdata1.js', description: 'testdata1.jsから', link: 'https://example.com/javascript' },
-    { title: 'test', description: 'testdata1.jsから', link: 'https://example.com/html' },
-    { title: 'j', description: 'testdata1.jsから', link: 'https://example.com/css' },
-    { title: 'test', description: 'test　testdata1.js', link: 'https://example.com/css' },
-    { title: 'APIの使い方', description: 'APIを使って外部データを取得する方法を学びます。testdata1.js', link: 'https://example.com/api' }
-];
