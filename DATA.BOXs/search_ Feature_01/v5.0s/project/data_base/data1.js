979bc097ed25052d3c5498e9ffce38177360196b
DATA.BOXs <satax726@gmail.com>
2024-09-29 16:24:26 +0900

    Add files via upload

---
+++ DATA.BOXs/search_ Feature_01/v5.0s/project/data_base/data1.js
@@ -0,0 +1,4 @@
+export const data1 = [
+    { title: 'JavaScript', description: '.data01.js' },
+    { title: 'あいうえお', description: 'あい' }
+];
---
+++ DATA.BOXs/search_ Feature_01/v5.0s/project/data_base/data3.js
@@ -0,0 +1,5 @@
+export const data3 = [
+    { title: 'hiugyutychtf', description: 'iojuhgytfrgfc' },
+    { title: 'えびもめ', description: 'えびもめ' }
+];
+
