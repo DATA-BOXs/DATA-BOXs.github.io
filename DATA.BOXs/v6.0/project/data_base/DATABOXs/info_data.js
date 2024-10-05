e56f137d57cebce28437712b87600fa3815211f9
DATA.BOXs <satax726@gmail.com>
2024-10-05 23:14:58 +0900

    Update info_data.js

--- DATA.BOXs/v6.0/project/data_base/DATABOXs/info_data.js
+++ DATA.BOXs/v6.0/project/data_base/DATABOXs/info_data.js
@@ -1,1 +1,5 @@
-
+export const info_data = [
+    { title: '間もなく、検索機能が追加されます。', description: '2024/9/29/16:10:45', link: 'https://sites.google.com/view/databoxs/data-boxs' },
+    { title: '検索機能v6.0をリリースしました。', description: '2024/10/5/23:13:35', link: 'https://sites.google.com/view/databoxs/data-boxs' }
+  
+];
