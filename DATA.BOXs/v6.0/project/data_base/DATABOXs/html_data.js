3c10376c6e7201a0e0e050cb9ca29192aa0065c1
DATA.BOXs <satax726@gmail.com>
2024-10-05 23:32:10 +0900

    Update html_data.js

--- DATA.BOXs/v6.0/project/data_base/DATABOXs/html_data.js
+++ DATA.BOXs/v6.0/project/data_base/DATABOXs/html_data.js
@@ -1,4 +1,21 @@
-export const html_data = [
+//export const html_data = [
     { title: 'HTTPステータスコード', description: 'HTTPステータスコードHTMLです', link: 'https://sites.google.com/view/databoxs/%E3%83%A6%E3%83%BC%E3%82%B6/dbs53735/d8' },
     { title: 'redirect', description: 'サイトの更新などに使用できます', link: 'https://sites.google.com/view/databoxs/%E3%83%A6%E3%83%BC%E3%82%B6/dbs53735/d3' }
+];
+
+export const html_data = [
+    {
+        title: 'HTTPステータスコード',
+        description: 'HTTPステータスコードHTMLです',
+        link: 'https://sites.google.com/view/databoxs/%E3%83%A6%E3%83%BC%E3%82%B6/dbs53735/d8',
+        additionalText: 'これはHTTPのステータスコードの一覧を説明するページです。',
+        imageUrl: 'https://example.com/path/to/image1.jpg'
+    },
+    {
+        title: 'redirect',
+        description: 'サイトの更新などに使用できます',
+        link: 'https://sites.google.com/view/databoxs/%E3%83%A6%E3%83%BC%E3%82%B6/dbs53735/d3',
+        additionalText: 'リダイレクトの仕組みと使い方を説明します。',
+        imageUrl: 'https://example.com/path/to/image2.jpg'
+    }
 ];
