c5182f77e669defee4dcfa9cda5a78ad0403cf31
DATA.BOXs <satax726@gmail.com>
2024-10-05 23:35:12 +0900

    Update html_data.js

--- DATA.BOXs/v6.0/project/data_base/DATABOXs/html_data.js
+++ DATA.BOXs/v6.0/project/data_base/DATABOXs/html_data.js
@@ -1,7 +1,7 @@
 //export const html_data = [
-    { title: 'HTTPステータスコード', description: 'HTTPステータスコードHTMLです', link: 'https://sites.google.com/view/databoxs/%E3%83%A6%E3%83%BC%E3%82%B6/dbs53735/d8' },
-    { title: 'redirect', description: 'サイトの更新などに使用できます', link: 'https://sites.google.com/view/databoxs/%E3%83%A6%E3%83%BC%E3%82%B6/dbs53735/d3' }
-];
+   // { title: 'HTTPステータスコード', description: 'HTTPステータスコードHTMLです', link: 'https://sites.google.com/view/databoxs/%E3%83%A6%E3%83%BC%E3%82%B6/dbs53735/d8' },
+   // { title: 'redirect', description: 'サイトの更新などに使用できます', link: 'https://sites.google.com/view/databoxs/%E3%83%A6%E3%83%BC%E3%82%B6/dbs53735/d3' }
+//];
 
 export const html_data = [
     {
