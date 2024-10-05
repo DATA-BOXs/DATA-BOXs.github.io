4be6c0705ffd428b0926f1d5ac6907a28679a661
DATA.BOXs <satax726@gmail.com>
2024-10-05 16:25:01 +0900

    Delete DATA.BOXs/search_ Feature_01/v5.0s/project directory

--- DATA.BOXs/search_ Feature_01/v5.0s/project/data_base/data1.js
+++
@@ -1,8 +0,0 @@
-//img
-export const data1 = [
-    { title: '立ち絵さたえっくすさん', description: 'よくわからん立ち絵です d1', link: 'https://sites.google.com/view/databoxs/%E3%83%A6%E3%83%BC%E3%82%B6/dbs53735/d1' },
-    { title: 'それっぽい背景', description: 'あーこれだってやつd4', link: 'https://sites.google.com/view/databoxs/%E3%83%A6%E3%83%BC%E3%82%B6/dbs53735/d4' },
-    { title: 'グラデーション(T)', description: 'グラデーションの縦Verd6', link: 'https://sites.google.com/view/databoxs/%E3%83%A6%E3%83%BC%E3%82%B6/dbs53735/d6' },
-    { title: 'グラデーション(Y)', description: 'グラデーションの横Verd7', link: 'https://sites.google.com/view/databoxs/%E3%83%A6%E3%83%BC%E3%82%B6/dbs53735/d7' },
-    { title: 'A', description: 'A', link: 'https://example.com/api' }
-];
