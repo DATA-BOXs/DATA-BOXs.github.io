9a0ab90da12af58c66c230c4842ea109f0b6fa68
DATA.BOXs <satax726@gmail.com>
2024-10-05 23:49:17 +0900

    Update html_data.js

--- DATA.BOXs/v6.0/project/data_base/DATABOXs/html_data.js
+++ DATA.BOXs/v6.0/project/data_base/DATABOXs/html_data.js
@@ -9,13 +9,13 @@ export const html_data = [
         description: 'HTTPステータスコードHTMLです',
         link: 'https://sites.google.com/view/databoxs/%E3%83%A6%E3%83%BC%E3%82%B6/dbs53735/d8',
         additionalText: 'HTMLsaucecode',
-        imageUrl: 'https://lh6.googleusercontent.com/GtuBnZveESLEKlTeHywCWoe-sZyM1f6fg7XSDf3uO9koMmboOWMGhR9Dv1gcAhs9i55GJPc8cNo-flyP3fmvXmG_UINPv11iu8fS9FlZwfkFPFDM2V-QHlrKF36Euxm9OQ=w1280'
+        imageUrl: 'https://DATA-BOXs.github.io/DATA/HTMLlog.png'
     },
     {
         title: 'redirect',
         description: 'サイトの更新などに使用できます',
         link: 'https://sites.google.com/view/databoxs/%E3%83%A6%E3%83%BC%E3%82%B6/dbs53735/d3',
         additionalText: 'HTMLsaucecode',
-        imageUrl: 'https://lh6.googleusercontent.com/GtuBnZveESLEKlTeHywCWoe-sZyM1f6fg7XSDf3uO9koMmboOWMGhR9Dv1gcAhs9i55GJPc8cNo-flyP3fmvXmG_UINPv11iu8fS9FlZwfkFPFDM2V-QHlrKF36Euxm9OQ=w1280'
+        imageUrl: 'https://DATA-BOXs.github.io/DATA/HTMLlog.png'
     }
 ];
