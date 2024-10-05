24a3faca12e7601ee9dda731e536fbb3c01b0601
DATA.BOXs <satax726@gmail.com>
2024-10-05 23:42:26 +0900

    Update html_data.js

--- DATA.BOXs/v6.0/project/data_base/DATABOXs/html_data.js
+++ DATA.BOXs/v6.0/project/data_base/DATABOXs/html_data.js
@@ -8,14 +8,14 @@ export const html_data = [
         title: 'HTTPステータスコード',
         description: 'HTTPステータスコードHTMLです',
         link: 'https://sites.google.com/view/databoxs/%E3%83%A6%E3%83%BC%E3%82%B6/dbs53735/d8',
-        additionalText: 'これはHTTPのステータスコードの一覧を説明するページです。',
-        imageUrl: 'https://example.com/path/to/image1.jpg'
+        additionalText: 'HTMLsaucecode',
+        imageUrl: 'https://lh6.googleusercontent.com/GtuBnZveESLEKlTeHywCWoe-sZyM1f6fg7XSDf3uO9koMmboOWMGhR9Dv1gcAhs9i55GJPc8cNo-flyP3fmvXmG_UINPv11iu8fS9FlZwfkFPFDM2V-QHlrKF36Euxm9OQ=w1280'
     },
     {
         title: 'redirect',
         description: 'サイトの更新などに使用できます',
         link: 'https://sites.google.com/view/databoxs/%E3%83%A6%E3%83%BC%E3%82%B6/dbs53735/d3',
-        additionalText: 'リダイレクトの仕組みと使い方を説明します。',
-        imageUrl: 'https://example.com/path/to/image2.jpg'
+        additionalText: 'HTMLsaucecode',
+        imageUrl: 'https://lh6.googleusercontent.com/GtuBnZveESLEKlTeHywCWoe-sZyM1f6fg7XSDf3uO9koMmboOWMGhR9Dv1gcAhs9i55GJPc8cNo-flyP3fmvXmG_UINPv11iu8fS9FlZwfkFPFDM2V-QHlrKF36Euxm9OQ=w1280'
     }
 ];
